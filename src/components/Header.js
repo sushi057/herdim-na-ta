c06426cd360f19f854f0f1d5282ff1f50f7f0389
suvash <suvash2077@gmail.com>
2023-05-02 17:13:43 +0545

    header initial design

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/header.css";
 import logo from "../images/logo.png";
+import searchButton from "../images/search-logo.png";
+import userButton from "../images/user-logo.png";
 
 const Header = () => {
   return (
@@ -10,14 +12,20 @@ const Header = () => {
         <Link to="/">
           <img src={logo} alt="main-logo" className="main-logo" />
         </Link>
+        <div className="links-left">
+          <Link to="/new">New</Link>
+          <Link to="/movies">Movies</Link>
+          <Link to="/shows">Shows</Link>
+          <Link to="/cartoon">Cartoon</Link>
+        </div>
       </div>
-      <div className="links">
-        <Link to="/new">New</Link>
-        <Link to="/movies">Movies</Link>
-        <Link to="/shows">Shows</Link>
-        <Link to="/cartoon">Cartoon</Link>
-        <Link to="/search">Search</Link>
-        <Link to="/user">User</Link>
+      <div className="links-right">
+        <Link to="/search">
+          <img src={searchButton} alt="search button" id="search-button" />
+        </Link>
+        <Link to="/user">
+          <img src={userButton} alt="user button" id="user-button" />
+        </Link>
       </div>
     </nav>
   );
