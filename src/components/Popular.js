02f9425759861e0656afb8f5747637dc373b00af
suvash <suvash2077@gmail.com>
2023-05-02 22:07:15 +0545

    Popular Initial Design

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -1,7 +1,30 @@
 import React from "react";
+import popularData from "../data/popularData";
+import "../styles/popular.css";
 
-function Popular() {
-  return <div>Popular</div>;
-}
+const Popular = () => {
+  console.log(popularData);
+  return (
+    <div className="popular-container">
+      <h2>Popular Shows</h2>
+      <div className="popular-list">
+        {popularData.map((show) => (
+          <div key={show.id} className="popular-item">
+            <div className="popular-item-image">
+              <img src={show.image} alt={show.name} />
+            </div>
+            <div className="popular-item-info">
+              <h3>{show.name}</h3>
+              <div className={`popular-item-rating bg-yellow`}>
+                {show.rating}
+              </div>
+              <div className="popular-item-seasons">{show.seasons} Seasons</div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Popular;
---
+++ src/data/popularData.js
@@ -0,0 +1,39 @@
+const popularData = [
+  {
+    id: 1,
+    name: "Pashupati Prasad",
+    image: "https://picsum.photos/id/1018/400/200",
+    rating: 8.7,
+    seasons: "1",
+  },
+  {
+    id: 2,
+    name: "Jatrai Jatra",
+    image: "https://picsum.photos/id/1020/400/200",
+    rating: 7.9,
+    seasons: "1",
+  },
+  {
+    id: 3,
+    name: "Kabaddi Kabaddi",
+    image: "https://picsum.photos/id/1024/400/200",
+    rating: 8.3,
+    seasons: "2",
+  },
+  {
+    id: 4,
+    name: "Chhakka Panja",
+    image: "https://picsum.photos/id/1022/400/200",
+    rating: 8.6,
+    seasons: "3",
+  },
+  {
+    id: 5,
+    name: "Loot",
+    image: "https://picsum.photos/id/1015/400/200",
+    rating: 8.2,
+    seasons: "1",
+  },
+];
+
+export default popularData;
