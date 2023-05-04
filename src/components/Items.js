6fc0b9e674733ff40c555047d95b0d92101a7483
suvash <suvash2077@gmail.com>
2023-05-04 15:08:48 +0545

    Intial Popular Design

---
+++ src/components/Items.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "../styles/items.css";
+
+const Item = ({ image, name, rating, seasons }) => {
+  return (
+    <div className="item-container">
+      <div className="item-image">
+        <img src={image} alt={name} />
+      </div>
+      <div className="item-info">
+        <div className="item-details">
+          <div className="item-rating">{rating}</div>
+          <div className="item-seasons">{seasons} Seasons</div>
+        </div>
+        <h3>{name}</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Item;
--- src/components/Popular.js
+++ src/components/Popular.js
@@ -1,28 +1,21 @@
 import React from "react";
 import popularData from "../data/popularData";
+import Items from "./Items";
 import "../styles/popular.css";
 
 const Popular = () => {
-  console.log(popularData);
   return (
     <div className="popular-container">
       <h2>Popular Shows</h2>
       <div className="popular-list">
         {popularData.map((show) => (
-          <div key={show.id} className="popular-item">
-            <div className="popular-item-image">
-              <img src={show.image} alt={show.name} />
-            </div>
-            <div className="popular-item-info">
-              <h3>{show.name}</h3>
-              <div className="popular-item-details">
-                <div className={"popular-item-rating"}>{show.rating}</div>
-                <div className="popular-item-seasons">
-                  {show.seasons} Seasons
-                </div>
-              </div>
-            </div>
-          </div>
+          <Items
+            key={show.id}
+            image={show.image}
+            name={show.name}
+            rating={show.rating}
+            seasons={show.seasons}
+          />
         ))}
       </div>
     </div>
