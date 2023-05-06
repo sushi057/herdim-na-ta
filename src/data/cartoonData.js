66006ef28e32a6ea77648cfd889a5aaf00951e30
suvash <suvash2077@gmail.com>
2023-05-06 07:37:58 +0545

    Data Modified

--- src/data/cartoonData.js
+++ src/data/cartoonData.js
@@ -1,43 +1,50 @@
 const cartoonData = [
   {
     name: "Cartoon Title",
-    image: "https://via.placeholder.com/200",
+    image:
+      "https://m.media-amazon.com/images/M/MV5BNTkzNjJhYjQtNjU0Yy00Y2M3LWI2ZDgtNDRhZmNlNDFjMjQ5XkEyXkFqcGdeQXVyODk2ODI3MTU@._V1_FMjpg_UX1000_.jpg",
     rating: 6.9,
     season: 4,
   },
   {
     name: "Cartoon Title",
-    image: "https://via.placeholder.com/200",
+    image:
+      "https://m.media-amazon.com/images/M/MV5BNTkzNjJhYjQtNjU0Yy00Y2M3LWI2ZDgtNDRhZmNlNDFjMjQ5XkEyXkFqcGdeQXVyODk2ODI3MTU@._V1_FMjpg_UX1000_.jpg",
     rating: 6.9,
     season: 4,
   },
   {
     name: "Cartoon Title",
-    image: "https://via.placeholder.com/200",
+    image:
+      "https://m.media-amazon.com/images/M/MV5BNTkzNjJhYjQtNjU0Yy00Y2M3LWI2ZDgtNDRhZmNlNDFjMjQ5XkEyXkFqcGdeQXVyODk2ODI3MTU@._V1_FMjpg_UX1000_.jpg",
     rating: 6.9,
     season: 4,
   },
   {
     name: "Cartoon Title",
-    image: "https://via.placeholder.com/200",
+    image:
+      "https://m.media-amazon.com/images/M/MV5BNTkzNjJhYjQtNjU0Yy00Y2M3LWI2ZDgtNDRhZmNlNDFjMjQ5XkEyXkFqcGdeQXVyODk2ODI3MTU@._V1_FMjpg_UX1000_.jpg",
     rating: 6.9,
     season: 4,
   },
   {
     name: "Cartoon Title",
-    image: "https://via.placeholder.com/200",
+    image:
+      "https://m.media-amazon.com/images/M/MV5BNTkzNjJhYjQtNjU0Yy00Y2M3LWI2ZDgtNDRhZmNlNDFjMjQ5XkEyXkFqcGdeQXVyODk2ODI3MTU@._V1_FMjpg_UX1000_.jpg",
     rating: 6.9,
     season: 4,
   },
   {
     name: "Cartoon Title",
-    image: "https://via.placeholder.com/200",
+    image:
+      "https://m.media-amazon.com/images/M/MV5BNTkzNjJhYjQtNjU0Yy00Y2M3LWI2ZDgtNDRhZmNlNDFjMjQ5XkEyXkFqcGdeQXVyODk2ODI3MTU@._V1_FMjpg_UX1000_.jpg",
     rating: 6.9,
     season: 4,
   },
   {
     name: "Cartoon Title",
-    image: "https://via.placeholder.com/200",
+    image:
+      "https://m.media-amazon.com/images/M/MV5BNTkzNjJhYjQtNjU0Yy00Y2M3LWI2ZDgtNDRhZmNlNDFjMjQ5XkEyXkFqcGdeQXVyODk2ODI3MTU@._V1_FMjpg_UX1000_.jpg",
     rating: 6.9,
     season: 4,
   },
--- src/data/tvData.js
+++ src/data/tvData.js
@@ -1,49 +1,57 @@
 const tvData = [
   {
     name: "Series Title",
-    image: "https://via.placeholder.com/200",
+    image:
+      "https://m.media-amazon.com/images/M/MV5BMDNkOTE4NDQtMTNmYi00MWE0LWE4ZTktYTc0NzhhNWIzNzJiXkEyXkFqcGdeQXVyMzQ2MDI5NjU@._V1_FMjpg_UX1000_.jpg",
     rating: 6.9,
     season: 4,
   },
   {
     name: "Series Title",
-    image: "https://via.placeholder.com/200",
+    image:
+      "https://m.media-amazon.com/images/M/MV5BMDNkOTE4NDQtMTNmYi00MWE0LWE4ZTktYTc0NzhhNWIzNzJiXkEyXkFqcGdeQXVyMzQ2MDI5NjU@._V1_FMjpg_UX1000_.jpg",
     rating: 6.9,
     season: 4,
   },
   {
     name: "Series Title",
-    image: "https://via.placeholder.com/200",
+    image:
+      "https://m.media-amazon.com/images/M/MV5BMDNkOTE4NDQtMTNmYi00MWE0LWE4ZTktYTc0NzhhNWIzNzJiXkEyXkFqcGdeQXVyMzQ2MDI5NjU@._V1_FMjpg_UX1000_.jpg",
     rating: 6.9,
     season: 4,
   },
   {
     name: "Series Title",
-    image: "https://via.placeholder.com/200",
+    image:
+      "https://m.media-amazon.com/images/M/MV5BMDNkOTE4NDQtMTNmYi00MWE0LWE4ZTktYTc0NzhhNWIzNzJiXkEyXkFqcGdeQXVyMzQ2MDI5NjU@._V1_FMjpg_UX1000_.jpg",
     rating: 6.9,
     season: 4,
   },
   {
     name: "Series Title",
-    image: "https://via.placeholder.com/200",
+    image:
+      "https://m.media-amazon.com/images/M/MV5BMDNkOTE4NDQtMTNmYi00MWE0LWE4ZTktYTc0NzhhNWIzNzJiXkEyXkFqcGdeQXVyMzQ2MDI5NjU@._V1_FMjpg_UX1000_.jpg",
     rating: 6.9,
     season: 4,
   },
   {
     name: "Series Title",
-    image: "https://via.placeholder.com/200",
+    image:
+      "https://m.media-amazon.com/images/M/MV5BMDNkOTE4NDQtMTNmYi00MWE0LWE4ZTktYTc0NzhhNWIzNzJiXkEyXkFqcGdeQXVyMzQ2MDI5NjU@._V1_FMjpg_UX1000_.jpg",
     rating: 6.9,
     season: 4,
   },
   {
     name: "Series Title",
-    image: "https://via.placeholder.com/200",
+    image:
+      "https://m.media-amazon.com/images/M/MV5BMDNkOTE4NDQtMTNmYi00MWE0LWE4ZTktYTc0NzhhNWIzNzJiXkEyXkFqcGdeQXVyMzQ2MDI5NjU@._V1_FMjpg_UX1000_.jpg",
     rating: 6.9,
     season: 4,
   },
   {
     name: "Series Title",
-    image: "https://via.placeholder.com/200",
+    image:
+      "https://m.media-amazon.com/images/M/MV5BMDNkOTE4NDQtMTNmYi00MWE0LWE4ZTktYTc0NzhhNWIzNzJiXkEyXkFqcGdeQXVyMzQ2MDI5NjU@._V1_FMjpg_UX1000_.jpg",
     rating: 6.9,
     season: 4,
   },
