8aa47286bd58608d9c7f93316dc905696f8ddeef
suvash <suvash2077@gmail.com>
2023-05-07 11:01:06 +0545

    polished css

--- src/data/popularData.js
+++ src/data/popularData.js
@@ -2,35 +2,39 @@ const popularData = [
   {
     id: 1,
     name: "Pashupati Prasad",
-    image: "https://picsum.photos/id/1018/400/200",
+    image:
+      "https://m.media-amazon.com/images/M/MV5BZjg4NjVhNWQtMjdlMC00ZGE4LWE1ZDUtYzdiMzE0ZTg1YzllXkEyXkFqcGdeQXVyNjY5NTY0NjM@._V1_.jpg",
     rating: 8.7,
     seasons: "1",
   },
   {
     id: 2,
     name: "Jatrai Jatra",
-    image: "https://picsum.photos/id/1020/400/200",
+    image:
+      "https://m.media-amazon.com/images/M/MV5BY2UwNTU4MGItOGVlNi00ZjRjLTliZWMtZTU1YTFmNDg0OTllXkEyXkFqcGdeQXVyNzQ3NTY5MjE@._V1_.jpg",
     rating: 7.9,
     seasons: "1",
   },
   {
     id: 3,
     name: "Kabaddi Kabaddi",
-    image: "https://picsum.photos/id/1024/400/200",
+    image:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/9/91/Kabaddi_Kabaddi.jpg/220px-Kabaddi_Kabaddi.jpg",
     rating: 8.3,
     seasons: "2",
   },
   {
     id: 4,
     name: "Chhakka Panja",
-    image: "https://picsum.photos/id/1022/400/200",
+    image:
+      "https://m.media-amazon.com/images/M/MV5BZWFiZjA3OGYtZjE1Yy00YWY1LThmZTgtODQ1NzRiNzAzZDY1XkEyXkFqcGdeQXVyNzQ3NTY5MjE@._V1_.jpg",
     rating: 8.6,
     seasons: "3",
   },
   {
     id: 5,
     name: "Loot",
-    image: "https://picsum.photos/id/1015/400/200",
+    image: "https://upload.wikimedia.org/wikipedia/en/c/c3/Loot_2012_film.jpg",
     rating: 8.2,
     seasons: "1",
   },
